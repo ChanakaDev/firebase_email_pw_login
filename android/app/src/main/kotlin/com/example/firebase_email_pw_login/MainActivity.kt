ba2f8717e20a1553a258a19508a7bd72362d35ad
ChanakaDev <mr.mbcnb@gmail.com>
2023-07-10 14:19:38 +0530

    Initial Commit

---
+++ android/app/src/main/kotlin/com/example/firebase_email_pw_login/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.firebase_email_pw_login
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,59 @@
+import 'package:flutter/material.dart';
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'package:get/get.dart';
+
+import 'views/login_screen.dart';
+import 'routes/routes.dart';
+import 'views/home_screen.dart';
+
+Future main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp();
+  runApp(const MyApp());
+}
+
+final navigatorKey = GlobalKey<NavigatorState>();
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return GetMaterialApp(
+      getPages: Routes.routes,
+      navigatorKey: navigatorKey,
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: const PageSwitcher(),
+    );
+  }
+}
+
+class PageSwitcher extends StatelessWidget {
+  const PageSwitcher({super.key});
+
+  @override
+  Widget build(BuildContext context) => Scaffold(
+        body: StreamBuilder(
+          stream: FirebaseAuth.instance.authStateChanges(),
+          builder: (context, snapshot) {
+            if (snapshot.connectionState == ConnectionState.waiting) {
+              return const Center(
+                child: CircularProgressIndicator(),
+              );
+            } else if (snapshot.hasError) {
+              return const Center(
+                child: Text('Something went wrong!'),
+              );
+            } else if (snapshot.hasData) {
+              return const HomeScreen();
+            } else {
+              return const LoginScreen();
+            }
+          },
+        ),
+      );
+}
